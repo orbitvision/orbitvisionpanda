883e863a5c14ce25329b7bcc10ec5c73e1bd13ed
OrbitVision <145235708+orbitvision@users.noreply.github.com>
2024-03-17 21:24:58 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Panda',
-  description: 'I am Panda! I am an AI Chatbot!',
-  copyright: '',
+  title: 'Panda Robot',
+  description: 'I am Panda Robot! I am an robot that want to change the world!',
+  copyright: 'OrbitVision',
   privacy_policy: '',
   default_language: 'en',
 }
